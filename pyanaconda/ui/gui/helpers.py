0afdea14bf8dafc76b359409a8ff674afaace778
Frederic Pierret (Epitre) <frederic.epitre@orange.fr>
2017-09-19 20:00:42 +0200

    anaconda: require user password being set
    Drop selectable option 'Require a password to use this account'. Make it required.
    
    QubesOS/qubes-issues#2574

--- pyanaconda/ui/gui/helpers.py
+++ pyanaconda/ui/gui/helpers.py
@@ -351,7 +351,7 @@ class GUISpokeInputCheckHandler(GUIInputCheckHandler, metaclass=ABCMeta):
     def check_password_confirm(self, inputcheck):
         """If the user has entered confirmation data, check whether it matches the password."""
         # Skip the check if no password is required
-        if (not self.input_enabled) or self.input_kickstarted:
+        if self.input_kickstarted:
             result = InputCheck.CHECK_OK
         elif self.input_confirmation and (self.input != self.input_confirmation):
             result = _(constants.PASSWORD_CONFIRM_ERROR_GUI)
@@ -371,7 +371,7 @@ class GUISpokeInputCheckHandler(GUIInputCheckHandler, metaclass=ABCMeta):
             return InputCheck.CHECK_OK
 
         # Skip the check if no password is required
-        if (not self.input_enabled) or self.input_kickstarted:
+        if self.input_kickstarted:
             return InputCheck.CHECK_OK
         # Also skip the check if the policy says that an empty password is fine
         # pylint: disable=no-member
@@ -413,7 +413,7 @@ class GUISpokeInputCheckHandler(GUIInputCheckHandler, metaclass=ABCMeta):
         self.set_input_status(status_text)
 
         # Skip the check if no password is required
-        if not self.input_enabled or self.input_kickstarted:
+        if self.input_kickstarted:
             return InputCheck.CHECK_OK
 
         # pylint: disable=no-member
