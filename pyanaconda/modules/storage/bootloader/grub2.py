fe1adbd542ac805301082585a4f2eeacb4b6043a
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2019-12-15 12:01:21 +0100

    add skip_grub parameter and allow boot encryption and the lvmlv format
    On coreboot systems, as grub2 can be used a payload it is possible to have all the partitions encrypted.
    
    Based on commits bf803e7 and 62cb1ca

--- pyanaconda/modules/storage/bootloader/grub2.py
+++ pyanaconda/modules/storage/bootloader/grub2.py
@@ -25,6 +25,7 @@ from pyanaconda.modules.storage.bootloader.base import BootLoader, BootLoaderErr
 from pyanaconda.core import util
 from pyanaconda.core.configuration.anaconda import conf
 from pyanaconda.core.i18n import _
+from pyanaconda.core.kernel import kernel_arguments
 from pyanaconda.product import productName
 
 from pyanaconda.anaconda_loggers import get_module_logger
@@ -129,6 +130,8 @@ class GRUB2(BootLoader):
     def __init__(self):
         super().__init__()
         self.encrypted_password = ""
+        self.encryption_support = True
+        self.skip_bootloader = kernel_arguments.get("skip_grub", False)
 
     #
     # configuration
@@ -186,7 +189,7 @@ class GRUB2(BootLoader):
         if productName.startswith("Red Hat "): # pylint: disable=no-member
             return ["xfs", "ext4", "ext3", "ext2"]
         else:
-            return ["ext4", "ext3", "ext2", "btrfs", "xfs"]
+            return ["ext4", "ext3", "ext2", "btrfs", "xfs", "lvmlv"]
 
     #
     # grub-related conveniences
